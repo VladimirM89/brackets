b363529941e9c84da347b9a23051bf04cf6b21f1
Vladimir <v.milashevsky@gmail.com>
2022-08-05 17:49:16 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [], bracketsMap = {};
+  for (let j = 0; j < bracketsConfig.length; j++) {
+  bracketsMap[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+
+    let curSymb = str[i];
+    let lastElem = stack[stack.length - 1];
+    if (bracketsMap[lastElem] === curSymb){
+      stack.pop()
+    } 
+    else {
+      stack.push(curSymb);
+    }
+  }
+
+  return stack.length === 0;
 }
