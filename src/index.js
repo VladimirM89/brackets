5dbce98a86c1ee97786e0c943f87382c04d18c76
Vladimir <v.milashevsky@gmail.com>
2022-08-05 19:44:34 +0300

    feat: fix mistakes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  let stack = [], bracketsMap = {};
+  let stack = [], bracketsObject = {};
   for (let j = 0; j < bracketsConfig.length; j++) {
-  bracketsMap[bracketsConfig[j][0]] = bracketsConfig[j][1];
+    bracketsObject[bracketsConfig[j][0]] = bracketsConfig[j][1];
   }
 
   for (let i = 0; i < str.length; i++) {
 
     let curSymb = str[i];
     let lastElem = stack[stack.length - 1];
-    if (bracketsMap[lastElem] === curSymb){
-      stack.pop()
+    if (bracketsObject[lastElem] === curSymb){
+      stack.pop();
     } 
     else {
       stack.push(curSymb);
